be244283797bc61a26116d5381aff52593e33260
Brendan Roberts <tarrnobi@users.noreply.github.com>
2017-10-07 16:05:17 +0100

    Add HexBoard component, set up Hello World Page

---
+++ hexaria/src/HexBoard.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class HexBoard extends Component {
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (<h1>Hello World</h1>);
+  }
+}
+
+export default HexBoard;
